c62464aff1d00e0885a794178ba29fa4b1b0d0fc
shashank <shashankskb18700@gmail.com>
2021-07-25 09:06:12 +0530

    season checker

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner.js';
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    
+    this.state = { lat: null ,erroMessage:''};
+
+  
+  }
+
+
+  componentDidMount() {
+     window.navigator.geolocation.getCurrentPosition(
+       (position) => this.setState({ lat: position.coords.latitude }),
+       (err) => this.setState({ erroMessage: err.message })
+     );
+  }
+
+  renderContent() {
+    if (this.state.lat && !this.state.erroMessage) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    } else if (this.state.erroMessage && !this.state.lat) {
+      return <div>{this.state.erroMessage}</div>;
+    }
+    return <Spinner message="please accept location request" />;
+
+  }
+  
+  render() {
+    return(
+    <div className="red">
+      {this.renderContent()}
+    </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <App />,
+  document.querySelector('#root')
+)
